1375504a717a8a1849d959bbb7c15ef4f372f908
Let Marçal <leticiamarcalmatias@gmail.com>
2021-10-07 19:30:11 -0300

    Criar classe ServicoFatura e métodos para entrega mínima

---
+++ src/com/br/zup/ServicoFatura.java
@@ -0,0 +1,18 @@
+package com.br.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoFatura {
+    private static List<Fatura> faturas = new ArrayList<>();
+
+    //-------------------------------------------------Método para cadastrar uma fatura
+    public static Fatura cadastrarFatura(String email, double valorDaFatura, String dataDeVencimento) throws Exception{
+        Consumidor consumidor = ServicoConsumidor.pesquisarConsumidorPorEmail(email);
+
+        Fatura fatura = new Fatura(consumidor, valorDaFatura, dataDeVencimento);
+        faturas.add(fatura);
+
+        return fatura;
+    }
+}
