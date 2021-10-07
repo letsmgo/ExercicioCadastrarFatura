0641b610209b00b99def3d8ae5ee400ff8b3c260
Let Marçal <leticiamarcalmatias@gmail.com>
2021-10-07 17:25:16 -0300

    Modelar classes

---
+++ src/com/br/zup/Consumidor.java
@@ -0,0 +1,56 @@
+package com.br.zup;
+
+import java.net.PortUnreachableException;
+
+public class Consumidor {
+    //-------------------------------------------------ATRIBUTOS
+    private String nome;
+    private String email;
+    private TipoDeCliente tipoDeCliente;
+
+    //-------------------------------------------------MÉTODO CONSTRUTOR
+    public Consumidor(){
+
+    }
+
+    public Consumidor(String nome, String email, TipoDeCliente tipoDeCliente) {
+        this.nome = nome;
+        this.email = email;
+        this.tipoDeCliente = tipoDeCliente;
+    }
+
+    //-------------------------------------------------GETTERS AND SETTERS
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoDeCliente getTipoDeCliente() {
+        return tipoDeCliente;
+    }
+
+    public void setTipoDeCliente(TipoDeCliente tipoDeCliente) {
+        this.tipoDeCliente = tipoDeCliente;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder dados = new StringBuilder();
+        dados.append("\nNome do consumidor: " + nome);
+        dados.append("\nE-mail: " + email);
+        dados.append("\nTipo de cliente: " + tipoDeCliente);
+
+        return dados.toString();
+    }
+}
---
+++ src/com/br/zup/Fatura.java
@@ -0,0 +1,54 @@
+package com.br.zup;
+
+public class Fatura {
+    //-------------------------------------------------ATRIBUTOS DA CLASSE
+    private Consumidor consumidor;
+    private double valorDaFatura;
+    private String dataDeVencimento;
+
+    //-------------------------------------------------MÉTODO CONSTRUTOR
+    public Fatura(Consumidor consumidor, double valorDaFatura, String dataDeVencimento) {
+        this.consumidor = consumidor;
+        this.valorDaFatura = valorDaFatura;
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    //-------------------------------------------------GETTERS AND SETTERS
+    public Consumidor getConsmidor() {
+        return consumidor;
+    }
+
+    public void setConsmidor(Consumidor consmidor) {
+        this.consumidor = consmidor;
+    }
+
+    public double getValorDaFatura() {
+        return valorDaFatura;
+    }
+
+    public void setValorDaFatura(double valorDaFatura) {
+        this.valorDaFatura = valorDaFatura;
+    }
+
+    public String getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(String dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    //-------------------------------------------------ToSTRING
+    @Override
+    public String toString() {
+        StringBuilder dados = new StringBuilder();
+        dados.append("\t\tINFORMAÇÕES DE FATURA: ");
+        dados.append("\nConsumidor: " + consumidor);
+        dados.append("\nValor: R$" + valorDaFatura);
+        dados.append("\nData de vencimento: " + dataDeVencimento);
+
+        return dados.toString();
+    }
+
+
+}
